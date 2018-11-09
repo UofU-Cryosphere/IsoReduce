a9ddabdbea1b4f4afdc91c0af94b5b226975f5e8
Durban G Keeler <durban.keeler@utah.edu>
2018-11-09 12:52:13 -0700

    Added comments for increased readability

--- memory.correct.R
+++ memory.correct.R
@@ -1,17 +1,19 @@
-## Custom function to estimate isotope ratios extrapolated from a power series optimized 
-## with the individual injection estimates for each sample. iso.data are all injections 
-## from a given sample
+
+# Custom function to correct isotope values of individual sample injection for memory
+# effects from the previous injections. 'iso.data' are all injections from a given sample
 memory.correct = function(iso.data) {
   
-  # If the max difference in values is below a given threshold, use geometric series 
-  # mixing model (avoids error in nonlinear least squares regression due to insubstantial
-  # differences)
+  # If the max difference in values is below a given threshold, do not correct injections
+  # for memory effects, but output the raw injection values
   if (abs(max(iso.data$d.18_16.Mean) - min(iso.data$d.18_16.Mean)) < 0.2) {
     d18O.method = as.character('Mean')
     d18O.correct = iso.data$d.18_16.Mean
     result = list(d18O.method, d18O.correct)
   }
   else {
+    
+    # Attempt memory correction using a power series optimized using the individual 
+    # injection estimates for each sample
     result = tryCatch(
       {
         d18O.method = as.character('Curve')
@@ -27,18 +29,12 @@ memory.correct = function(iso.data) {
         b = d18O.summ$coefficients[2,1]
         e = a*b*20^(b+1)
         d18O.correct = iso.data$d.18_16.Mean - a*b*iso.data$Inj.Nr^(b+1) + e
-        # 
-        # d18O.method = as.character('Curve')
-        # d18O.curve = nls(d.18_16.Mean ~ a*Inj.Nr^(b), data = iso.data,
-        #                start = list(a = head(iso.data$d.18_16.Mean, 1) 
-        #                             - tail(iso.data$d.18_16.Mean, 1), b = -2))
-        # d18O.summ = summary(d18O.curve)
-        # a = d18O.summ$coefficients[1,1]
-        # b = d18O.summ$coefficients[2,1]
-        # e = a*20^b
-        # d18O.correct = iso.data$d.18_16.Mean - a*iso.data$Inj.Nr^b + e
+        
         result = list(d18O.method, d18O.correct)
       },
+      
+      # If the power series model fails for any reason, switch to using a geometric
+      # series mixing model instead
       error = function(err) {
         d18O.method = as.character('Mixing')
         b = -1
@@ -66,12 +62,20 @@ memory.correct = function(iso.data) {
   d18O.predict = mean(d18O.correct)
   d18O.sigma = sd(d18O.correct)
   
+  
+  ## Same subroutine as that performed on d18O values, applied to the dD values
+  
+  # If the max difference in values is below a given threshold, do not correct injections
+  # for memory effects, but output the raw injection values
   if (abs(max(iso.data$d.D_H.Mean) - min(iso.data$d.D_H.Mean)) < 0.2) {
     dD.method = as.character('Mean')
     dD.correct = iso.data$d.D_H.Mean
     result = list(dD.method, dD.correct)
   }
   else {
+    
+    # Attempt memory correction using a power series optimized using the individual 
+    # injection estimates for each sample
     result = tryCatch(
       { 
         dD.method = as.character('Curve')
@@ -88,18 +92,11 @@ memory.correct = function(iso.data) {
         e = a*b*20^(b+1)
         dD.correct = iso.data$d.D_H.Mean - a*b*iso.data$Inj.Nr^(b+1) + e
         
-        # dD.method = as.character('Curve')
-        # dD.curve = nls(d.D_H.Mean ~ a*Inj.Nr^(b), data = iso.data,
-        #                start = list(a = head(iso.data$d.D_H.Mean, 1) 
-        #                             - tail(iso.data$d.D_H.Mean, 1), b = -2))
-        # dD.summ = summary(dD.curve)
-        # a = dD.summ$coefficients[1,1]
-        # b = dD.summ$coefficients[2,1]
-        # e = a*20^b
-        # dD.correct = iso.data$d.D_H.Mean - a*iso.data$Inj.Nr^b + e
-        # result = list(dD.method, dD.correct)
         result = list(dD.method, dD.correct)
       },
+      
+      # If the power series model fails for any reason, switch to using a geometric
+      # series mixing model instead
       error = function(err) {
         dD.method = as.character('Mixing')
         b = -1
@@ -133,6 +130,8 @@ memory.correct = function(iso.data) {
   # plot(iso.data$d.D_H.Mean)
   # points(dD.correct, col = 'red')
   
+  # Return results as a list of final method used, corrected isotope values and estimated
+  # error on correction for d18O and dD values
   output = list(d18O.method, d18O.predict, d18O.sigma, dD.method, dD.predict, dD.sigma)
   return(output)
 }
