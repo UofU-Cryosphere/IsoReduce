32f56c9990fac10b477d9f442f45b11af583d99e
Durbank <32938050+durbank@users.noreply.github.com>
2019-06-05 16:32:11 -0600

    Updated data directory structure (#2)
    * Modified scripts and structures for more modular data directory structures
    
    * Additional modifications to bring functions in line with directory structure changes
    
    * Modified to ignore Rnotebook and similar related output files
    
    * Removed generated outputs
    
    * Added cleaned version of field notes data
    
    * Reduces data and filters/combines relevant data for later analysis
    
    * Utilizes here() for relative file paths; updates to use the tidyverse

--- R-functions/iso.loop.R
+++ R-functions/iso.loop.R
@@ -1,10 +1,10 @@
 ## For loop function to calculate mean isotope values for all samples using iso.curve function
 
 iso.loop = function(data.all) {
-  
+
   # Source the memory correction function
-  source('R-functions/memory.correct.R')
-  
+  source(here("R-functions", "memory.correct.R"))
+
   # Preallocate arrays for reduced data
   Sample.port = numeric()
   d18O.method = character()
@@ -13,12 +13,12 @@ iso.loop = function(data.all) {
   dD.method = character()
   dD.predict = numeric()
   dD.sigma = numeric()
-  
+
   # Define starting values for use in for loop
   matrix.row = 1
   sample.nr = data.all$Sample[1]
   sample.row = 1
-  
+
   # Iteratively calculate final estimate for isotope ratios for each sample
   for (i in 1:nrow(data.all)) {
     if (data.all$Sample[i] != sample.nr) { # Execute after change in sample number
@@ -34,7 +34,7 @@ iso.loop = function(data.all) {
       sample.row = i
       sample.nr = data.all$Sample[i]
       matrix.row = matrix.row+1
-    } 
+    }
     else if (i == nrow(data.all)) { # Execute if final row of data
       data.i = data.all[sample.row:i,]
       output = memory.correct(data.i)
@@ -48,12 +48,12 @@ iso.loop = function(data.all) {
       #data.reduce[matrix.row,] = c(sample.nr, iso.curve(data.i))
     }
   }
-  
+
   # Place reduced data into data frame (numeric and factor classes)
-  data.reduce = data.frame(Sample.port = as.numeric(Sample.port), d18O.method = 
-                             factor(d18O.method, c('Curve', 'Mixing', 'Mean')), d18O.predict = 
-                             as.numeric(d18O.predict), d18O.sigma = as.numeric(d18O.sigma), 
-                           dD.method = factor(dD.method, c('Curve', 'Mixing', 'Mean')), dD.predict = 
+  data.reduce = data.frame(Sample.port = as.numeric(Sample.port), d18O.method =
+                             factor(d18O.method, c('Curve', 'Mixing', 'Mean')), d18O.predict =
+                             as.numeric(d18O.predict), d18O.sigma = as.numeric(d18O.sigma),
+                           dD.method = factor(dD.method, c('Curve', 'Mixing', 'Mean')), dD.predict =
                              as.numeric(dD.predict), dD.sigma = as.numeric(dD.sigma))
   return(data.reduce)
 }
--- R-functions/iso.reduce.R
+++ R-functions/iso.reduce.R
@@ -1,46 +1,47 @@
 # Function for reducing isotope data within a given folder, based on a given tray template
 iso.reduce = function(files.paths, template.path) {
-  
+
   # Source data reduction functions
-  source('R-functions/iso.combine.R')
-  source('R-functions/iso.loop.R')
-  source('R-functions/drift.correct.R')
-  source('R-functions/bias.correct.R')
-  source('R-functions/template.read.R')
-  
+  source(here('R-functions', 'iso.combine.R'))
+  source(here('R-functions', 'iso.loop.R'))
+  source(here('R-functions', 'drift.correct.R'))
+  source(here('R-functions', 'bias.correct.R'))
+  source(here('R-functions', 'template.read.R'))
+
   # Concatenates an arbitrary number of Picarro .csv files into a single R dataframe
   data.all = iso.combine(files.paths)
-  
+
   # Loop through sample injections to correct the data for sample memory effects
   data.reduce = iso.loop(data.all)
-  
+
   # Load template-specific data (location of standards/QC, standards values, etc.)
   template.data = template.read(template.path)
   STND.loc = template.data[[1]]
   STND.val = template.data[[2]]
   QC.loc = template.data[[3]]
   QC.val = template.data[[4]]
-  
+  IDs = template.data[[5]]
+
   # Drift correct isotope data
   drift.correction = drift.correct(data.reduce, STND.loc)
   d18O.drift.correct = unlist(drift.correction[1])
   dD.drift.correct = unlist(drift.correction[2])
-  
-  
+
+
   STND.idx = vector(mode = "list", length = length(STND.loc))
   for (i in 1:length(STND.loc)) {
     STND.idx[[i]] = match(STND.loc[[i]], data.reduce$Sample.port)
   }
-  
-  
-  
+
+
+
   # Correct linear offset in isotope data using values of known standards
   bias.correction = bias.correct(d18O.drift.correct, dD.drift.correct, STND.idx, STND.val)
-  
+
   # Add drift- and offset- corrected data to 'data.reduce', and reorder for easier viewing
   data.reduce$d18O.correct = unlist(bias.correction[1])
   data.reduce$dD.correct = unlist(bias.correction[2])
-  data.reduce = data.reduce[c('Sample.port', 'd18O.correct', 'd18O.sigma', 'd18O.method', 
+  data.reduce = data.reduce[c('Sample.port', 'd18O.correct', 'd18O.sigma', 'd18O.method',
                               'd18O.predict', 'dD.correct', 'dD.sigma', 'dD.method','dD.predict')]
 
   # # Diagnostics to check drif corrections
@@ -50,15 +51,35 @@ iso.reduce = function(files.paths, template.path) {
   # plot(d18O.drift.correct[unlist(STND.loc$P.mid)])
   # plot(d18O.drift.correct[unlist(STND.loc$P.zero)])
   # plot(d18O.drift.correct[unlist(STND.loc$P.depl)])
-  d18O.error = (data.reduce$d18O.correct[match(QC.loc, data.reduce$Sample.port)] - QC.val[1])
-  dD.error = (data.reduce$dD.correct[match(QC.loc, data.reduce$Sample.port)] - QC.val[2])
-  
-  
-  # Compare corrected QC sample values to their true values
-  
-  
-  
-  
-  
+
+
+  ## Compare corrected QC sample values to their true values
+
+  # Get index of QC samples within data
+  QC.idx = match(QC.loc, data.reduce$Sample.port)
+
+
+  d18O.error = rep(0, times = length(QC.idx))
+  dD.error = rep(0, times = length(QC.idx))
+  if (length(QC.idx) > 1) {
+    for (i in 1:length(QC.idx)) {
+
+      QC.i = QC.val[i,]
+      d18O.error[i] = data.reduce$d18O.correct[QC.idx[i]] - QC.i[1]
+      dD.error[i] = data.reduce$dD.correct[QC.idx[i]] - QC.i[2]
+    }
+  } else {
+
+    d18O.error[i] = data.reduce$d18O.correct[QC.idx] - QC.val[1]
+    dD.error[i] = data.reduce$dD.correct[QC.idx] - QC.val[2]
+  }
+
+  # Convert reduced data to tibble format and combine with Sample numbers and IDs
+  data.reduce = as_tibble(data.reduce) %>%
+    left_join(IDs, by = c("Sample.port" = "Port_num"))
+
+
+
+
   return(data.reduce)
 }
