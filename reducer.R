8d9fa6065a479de457a1cb3e4915a90bccfe2a65
Durban G. Keeler <durban.keeler@gmail.com>
2019-10-21 12:45:33 -0600

    Updates wrapper script for class use; pulls templates out of individual folders

--- reducer.R
+++ reducer.R
@@ -1,19 +1,16 @@
 # Script used to reduce isotope data based on the custom function 'iso.reduce'
 
+# Import required libraries
+library(tidyverse)
+library(here)
+
+# Select the .csv file containing the final raw data as outputed by the Picarro
+files.paths = file.choose()
 
 
-# # Set working directory to location of this source file (the 'reducer.R' wrapper script)
-# script.dir = dirname(parent.frame(2)$ofile)
-# setwd(script.dir)
-
-# Input the directory containing the raw isotope data (subdirectory of 'Raw data')
-files.paths = choose.files(caption = "Select .csv files of raw isotope data")
-# files.paths = choose.files(caption = "Select .csv files of raw isotope data", 
-#                            filters = Filters[".csv"])
-
-# Input the .Rdata file that contains the tray template sample/standards locations
-template.path = choose.files(caption = 'Select Excel sample tray template file')
+# Select the .xlsx file containing the properly formatted and standardized tray template for this run
+template.path = file.choose()
 
 # Source and run the isotope reduction functions on given input files
 source('R-functions/iso.reduce.R')
-data = iso.reduce(files.paths, template.path)
+reduced.data = iso.reduce(files.paths, template.path)
