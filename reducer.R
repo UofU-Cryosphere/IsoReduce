ce1290f11f2008a4ad5a4531572c63863eb76caf
Durban G. Keeler <durban.keeler@gmail.com>
2019-11-12 17:52:54 -0700

    Class updates and changes

--- reducer.R
+++ reducer.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(readxl)
 library(here)
+library(plotly)
 
 # Select the .csv file containing the final raw data as outputed by the Picarro
 files.paths = file.choose()
@@ -17,7 +18,8 @@ source('R-functions/iso.reduce.R')
 reduced.data = iso.reduce(files.paths, template.path)
 
 # Plot d18O values of reduced data
-reduced.data %>% ggplot(., aes(x=1:nrow(.), y=d18O.correct)) + geom_point()
+d18O.plt = reduced.data %>% ggplot(., aes(x=1:nrow(.), y=d18O.correct, label = Sample_ID)) + geom_point()
+ggplotly(d18O.plt)
 
 # Plot dD values of reduced data
 reduced.data %>% ggplot(., aes(x=1:nrow(.), y=dD.correct)) + geom_point()
